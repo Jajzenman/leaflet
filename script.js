499908749392838f30e5145a4c56d2f73a43bbc1
Jajzenman <jajzenman@gmail.com>
2025-03-31 16:19:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let poemMap;
+
+// Initialize the map
+poemMap = L.map('map');
+
+// Add a tile layer to the map
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(poemMap);
+poemMap.setView([40.7128, -74.0060], 16);
+
+// Example marker
+const timeSquare = L.marker([51.505, -0.09]).addTo(poemMap)
+    .bindPopup('A poetic marker!')
+    .openPopup();
